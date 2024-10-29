ed172c6ad1d20310edcbfd4dc644f21e7c06bf8b
kiku-hub <tacyskani@gmail.com>
2024-10-29 11:41:24 +0900

    商品出品ページの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,31 @@
 class ItemsController < ApplicationController
+  # 特定のアクションに対してユーザー認証を適用
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
-    # 必要に応じて処理を記述
+    # トップページ用の処理（必要に応じて記述）
+  end
+
+  # 商品出品ページを表示するアクション
+  def new
+    @item = Item.new
+  end
+
+  # 商品情報を保存するアクション
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '出品が完了しました。'
+    else
+      render :new # エラーハンドリング: 失敗した場合は出品ページに戻る
+    end
+  end
+
+  private
+
+  # ストロングパラメータ
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :region_id,
+                                 :shipping_day_id, :price)
   end
 end
