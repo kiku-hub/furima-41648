f8514e676c28c267ff6dbd3c166630817242b66c
kiku-hub <tacyskani@gmail.com>
2024-11-01 17:54:30 +0900

    フォームの情報を保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,35 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :set_regions, only: [:index, :create]
+
   def index
     @order_form = OrderForm.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
-    @order_form = OrderForm.new(order_params)
+    @order_form = OrderForm.new(order_params.merge(user_id: current_user.id, item_id: @item.id))
 
     if @order_form.save
       redirect_to root_path, notice: '購入が完了しました'
     else
-      @item = Item.find(params[:item_id])
+      # バリデーションエラーのメッセージを取得
+      flash.now[:alert] = @order_form.errors.full_messages.join(', ')
       render :index
     end
   end
 
   private
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def set_regions
+    @regions = Region.all
+  end
+
   def order_params
-    params.require(:order_form).permit(:user_id, :item_id, :postal_code, :region_id, :city, :house_number, :building_name,
-                                       :phone_number)
+    params.require(:order_form).permit(:postal_code, :region_id, :city, :house_number,
+                                       :building_name, :phone_number) # user_idとitem_idはここから除外
   end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -3,6 +3,7 @@ class OrderForm
 
   attr_accessor :user_id, :item_id, :postal_code, :region_id, :city, :house_number, :building_name, :phone_number
 
+  # バリデーション
   validates :user_id, :item_id, :postal_code, :region_id, :city, :house_number, :phone_number, presence: true
   validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'はXXX-XXXXの形式で入力してください' }
   validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は10桁または11桁の数字で入力してください' }
@@ -10,11 +11,17 @@ class OrderForm
   def save
     return false unless valid?
 
-    # Orderテーブルにデータを保存
-    order = Order.create(user_id: user_id, item_id: item_id)
+    ActiveRecord::Base.transaction do
+      # Orderテーブルにデータを保存
+      order = Order.create!(user_id: user_id, item_id: item_id) # create!を使用してエラーを発生させる
+      # Addressテーブルにデータを保存
+      Address.create!(postal_code: postal_code, region_id: region_id, city: city, house_number: house_number,
+                      building_name: building_name, phone_number: phone_number, order_id: order.id)
+    end
 
-    # Addressテーブルにデータを保存
-    Address.create(postal_code: postal_code, region_id: region_id, city: city, house_number: house_number,
-                   building_name: building_name, phone_number: phone_number, order_id: order.id)
+    true # 正常に保存が完了したらtrueを返す
+  rescue ActiveRecord::RecordInvalid => e
+    errors.add(:base, "保存に失敗しました: #{e.message}") # エラーメッセージを追加
+    false # 保存が失敗した場合はfalseを返す
   end
 end
